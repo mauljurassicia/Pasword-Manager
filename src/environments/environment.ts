7fe9421b960abe878a1074c3933746d48610c999
Maulana <smamalik33@gmail.com>
2023-07-15 20:48:16 +0700

    commit is iniated

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'password-manager-d40dd',
+    appId: '1:415182712905:web:994aa2c3297a02a2b5cf98',
+    storageBucket: 'password-manager-d40dd.appspot.com',
+    apiKey: 'AIzaSyBB9DBsmnYl3268xGN6ffG_D9M7puj7KEw',
+    authDomain: 'password-manager-d40dd.firebaseapp.com',
+    messagingSenderId: '415182712905',
+  },
+    production: false
+}
