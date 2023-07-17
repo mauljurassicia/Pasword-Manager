74250b8124aace0eeb5b826df2d045ea88901c81
Maulana <smamalik33@gmail.com>
2023-07-17 14:59:28 +0700

    recent update

--- src/app/password-manager.service.ts
+++ src/app/password-manager.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class PasswordManagerService {
 
-  constructor() { }
+  constructor( private firestore: Firestore) { 
+  }
+
+  addsite(data: Object){
+    const dbInstance = collection(this.firestore, 'sites');
+    return addDoc(dbInstance, data);
+  }
+
+  loadSites(){
+    const dbInstance = collection(this.firestore, 'sites');
+    return collectionData(dbInstance, {idField: 'id'});
+  }
 }
--- src/app/site-list/site-list.component.ts
+++ src/app/site-list/site-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PasswordManagerService } from '../password-manager.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-site-list',
@@ -7,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class SiteListComponent {
 
+  allSites !: Observable<Array<any>>
+
+  siteName !: string;
+  siteUrl !: string;
+  siteImgUrl !: string;
+  siteId !: string;
+
+  formState : string = "";
+
+  constructor(private passwordManagerService: PasswordManagerService){
+    this.loadSites();
+  }
+
+  onSubmit(values: object){
+    console.log(values);
+    this.passwordManagerService.addsite(values).then(() =>{console.log('Data saved successfully')})
+    .catch((err) => { console.log(err)});
+  }
+
+  loadSites(){
+    this.allSites = this.passwordManagerService.loadSites();
+  }
+
+  editSite(siteName: string, siteUrl: string, siteImgUrl: string, id: string){
+    this.siteName = siteName;
+    this.siteUrl = siteUrl;
+    this.siteImgUrl = siteImgUrl;
+    this.siteId = id;
+  }
 }
